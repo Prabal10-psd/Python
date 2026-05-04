87c44b8aba68c23a79577de9b4d6646f1748286b
Prabal <nooblekill123@gmail.com>
2026-05-04 18:45:00 +0545

    Country Added

--- first.py
+++ first.py
@@ -2,4 +2,5 @@ name = input("Enter your name: ")
 age = int(input("Enter your age: "))
 year = 2025 + (18 - age)
 print(f"Hello {name}! You will turn 18 in the year {year}.")
-
+country = input("Enter your country: ")
+print(f"You are from {country}. Nice to meet you, {name}!")
